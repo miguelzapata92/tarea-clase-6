127c8d5e76c611ef7223ad55440360d4985be062
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2020-11-11 19:52:41 -0300

    tarea-clase-6

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -1,3 +1,5 @@
+//guardo el boton cargar
+
 const $botonCargar = document.querySelector("#cargar-integrantes")
 
 $botonCargar.onclick = function() {
@@ -8,18 +10,23 @@ $botonCargar.onclick = function() {
 
 
     for( i = 0; i < $numeroIntegrantes; ++i){
+
+        //llamo a la funcion que se ejecuta
         
         listaIntegrante.appendChild(crearIntegrante())
         
 
     }
 
-    
+    document.querySelector("#boton-calcular").style.display = "block";
+
 
 
     return false
 
 }
+
+//describo la función
 
 function crearIntegrante() {
     let integrante = document.createElement("li")
@@ -27,11 +34,12 @@ function crearIntegrante() {
     let labelNombre = document.createElement("label")
     labelNombre = document.createTextNode("Nombre: ")
     let nombre = document.createElement("input")
-    nombre.type = "text";
+    nombre.type = "text"
+    nombre.className = ("nombresIntegrantes")
     let labelEdad = document.createElement("label")
     labelEdad = document.createTextNode("Edad: ")
     let edad = document.createElement("input")
-    edad.className = "label-edades"
+    edad.className = "edadesIntegrantes"
     edad.type = "number"
 
     integrante.appendChild(labelNombre)
@@ -41,3 +49,57 @@ function crearIntegrante() {
 
     return integrante
 }
+
+const divResultado = document.querySelector("#resultado")
+
+const $botonCalcularEdades = document.querySelector("#boton-calcular")
+
+$botonCalcularEdades.onclick = function() {
+
+    const $edades = document.querySelectorAll(".edadesIntegrantes")
+    
+    const $nombres = document.querySelectorAll(".nombresIntegrantes")
+
+    document.querySelector("#edad-mas-grande").innerText = calcularEdadMayor($edades)
+
+    return false
+
+}
+
+function calcularEdadMayor($edades) {
+
+    //no funciona porque el ... es para los arrays, corregir
+
+   let $edadMasGrande = Math.max(...$edades)
+
+   return `El de mayor edad tiene ${$edadMasGrande} años`
+
+}
+
+/*
+    //cargo en una constante las edades que se cargan
+
+    const $arrayInput = document.querySelectorAll(".edadesIntegrantes").value
+
+    //creo el array vacío donde voy a cargar las edades
+    let $arrayEdades = []
+
+    for(i = 0; i < $arrayInput.length; i++) {
+        $arrayEdades.push(Number($arrayInput[i].innerText))
+    }
+
+    let $edadMasGrande = Math.max(...$arrayEdades)
+
+    resultado1 = document.querySelector("edad-mas-grande")
+
+    resultado1.innerText = $edadMasGrande
+
+    let $edadMasChica = Math.min(...$arrayEdades)
+
+    resultado2 = document.querySelector("#edad-mas-chica")
+
+    resultado2.innerText = $edadMasChica
+
+    return false
+    
+}*/
