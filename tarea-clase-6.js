9083439b49db8e0fb7f470f92d116b905706540e
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2020-11-13 12:07:26 -0300

    tarea-clase-6

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -31,15 +31,17 @@ $botonCargar.onclick = function() {
 function crearIntegrante() {
     let integrante = document.createElement("li")
     integrante.className = "integrantes"
+    
     let labelNombre = document.createElement("label")
     labelNombre = document.createTextNode("Nombre: ")
     let nombre = document.createElement("input")
     nombre.type = "text"
     nombre.className = ("nombresIntegrantes")
+    
     let labelEdad = document.createElement("label")
     labelEdad = document.createTextNode("Edad: ")
     let edad = document.createElement("input")
-    edad.className = "edadesIntegrantes"
+    edad.className = "edades"
     edad.type = "number"
 
     integrante.appendChild(labelNombre)
@@ -56,26 +58,65 @@ const $botonCalcularEdades = document.querySelector("#boton-calcular")
 
 $botonCalcularEdades.onclick = function() {
 
-    const $edades = document.querySelectorAll(".edadesIntegrantes")
-    
-    const $nombres = document.querySelectorAll(".nombresIntegrantes")
+    const $edades = document.querySelectorAll(".edades")
 
-    document.querySelector("#edad-mas-grande").innerText = calcularEdadMayor($edades)
+    const edades = []
+
+    for(i = 0; i < $edades.length; i++) {
+        edades.push(Number($edades[i].value))
+    }
+    
+
+    document.querySelector("#edad-mas-grande").innerText = calcularEdadMayor(edades)
+
+    document.querySelector("#edad-mas-chica").innerText = calcularEdadMenor(edades)
+
+    document.querySelector("#edad-promedio").innerText = calcularPromedioEdad(edades)
 
     return false
 
 }
 
-function calcularEdadMayor($edades) {
 
-    //no funciona porque el ... es para los arrays, corregir
 
-   let $edadMasGrande = Math.max(...$edades)
-
-   return `El de mayor edad tiene ${$edadMasGrande} años`
-
+function calcularEdadMayor(edades) {
+    let edadMayor = Math.max(...edades)
+    
+    return `el más viejito tiene ${edadMayor} años`
 }
 
+
+
+function calcularEdadMenor(edades) {
+    let edadMenor = Math.min(...edades)
+
+    return `el más jóven tiene ${edadMenor} años`
+}
+
+function calcularPromedioEdad(edades) {
+    totalDeEdades = 0
+    for(i = 0; i < edades.length; i++) {
+        totalDeEdades += edades[i]
+    }
+    promedio = totalDeEdades/edades.length
+
+    return `El promedio de edad es de ${promedio} años`
+}
+
+
+
+
+/*
+let mayorNumero = numeros[0];
+for (let i = 1; i < numeros.length; i++) {
+  if (numeros[i] > mayorNumero) {
+    mayorNumero = numeros[i];
+  }
+}
+
+return mayorNumero;
+}
+*/
 /*
     //cargo en una constante las edades que se cargan
 
